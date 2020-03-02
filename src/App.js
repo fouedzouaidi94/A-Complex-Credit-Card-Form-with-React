4fabd266aa09f9cea483c638a44d9659caa163e6
foued zouaidi <foued.zouaidi94@gmail.com>
2020-03-02 10:55:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./components/card";
+// import "react-credit-cards/es/styles-compiled.css";
+import "./components/style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
